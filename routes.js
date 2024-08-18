6ca94b25103655c3c57fb33b4f210d266380612d
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-08-18 17:51:31 -0300

    feat: create route for allcars get

--- routes.js
+++ routes.js
@@ -2,10 +2,13 @@ const express = require("express")
 const route = express.Router()
 const homeController = require("./src/controllers/homeController")
 const registerCarController = require("./src/controllers/registerCarController")
+const allCarsController = require("./src/controllers/allCarsController")
 
 route.get("/", homeController.home)
 
 route.get("/registercar", registerCarController.registerCar)
 route.post("/registercar", registerCarController.registerCarPost)
+
+route.get("/allcars", allCarsController.listAllCars)
 
 module.exports = route
