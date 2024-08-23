eab26d097c0be4ed1e7287935d60364dcc57091e
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-08-23 11:39:54 -0300

    feat: add  view and template for /allcars/

--- src/controllers/allCarsController.js
+++ src/controllers/allCarsController.js
@@ -1,3 +1,9 @@
-exports.listAllCars = (req, res) => {
-    res.render("allCars.hbs")
+const mongoose = require("mongoose")
+const CarModel = require("../models/carModel")
+
+exports.listAllCars = async (req, res) => {
+    const cars = await CarModel.find()
+    const newCars = cars.map(car => car.toObject())
+    console.log(cars)
+    res.render("allCars.hbs", { newCars })
 }
