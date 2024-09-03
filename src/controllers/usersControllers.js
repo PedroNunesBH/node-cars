df1cfdf8090b9cbb85468eeda0e6c00984ce8ddc
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-03 18:11:51 -0300

    feat: add logic to allUsersGet list users

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -2,5 +2,7 @@ const mongoose = require("mongoose")
 const UserModel = require("../models/userModel.js")
 
 exports.allUsersGet = (async (req, res) => {
-    res.render("allUsers")
+    const allUsers = await UserModel.find()
+    const allUsersObjects = allUsers.map(user => user.toObject())
+    res.render("allUsers", { allUsersObjects })
 })
