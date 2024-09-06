6a988fb65c811efee3f8c09152f8382b7615950e
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-06 11:37:09 -0300

    feat: add user login logic to home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,22 @@
+const bcrypt = require("bcryptjs/dist/bcrypt")
+const UserModel = require("../models/userModel")
+
 exports.home = (req, res) => {
     res.render("index.hbs")
+}
+
+exports.homePost = async (req, res) => {
+    const {username, password} = req.body
+
+    try {
+        const user = await UserModel.findOne({username: username})
+        if (user && await bcrypt.compare(password, user.password)) {
+            req.session.user = user
+            res.redirect("/allcars")
+    } else { 
+        res.redirect("/")
+    }
+    } catch (error) {
+        res.redirect("/")
+    }
 }
--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -3,6 +3,7 @@ const route = express.Router()
 const homeController = require("../controllers/homeController")
 
 route.get("/", homeController.home)
+route.post("/", homeController.homePost)
 
 
 module.exports = route
