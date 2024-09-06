06265d322e1a19ab588ae7225c52e89f4f9fc583
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-06 12:17:01 -0300

    fix: update authMiddleware to set user in res.locals

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,7 +1,9 @@
 function confirmAuthentication (req, res, next) {
     if (req.session && req.session.user) {
+        res.locals.user = req.session.user
         return next()
     } else {
+        res.locals.user = null
         res.redirect("/")
     }
 }
