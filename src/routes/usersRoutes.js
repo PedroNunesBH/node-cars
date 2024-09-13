e064349104b0511067ef3af3fcc093357cef7d54
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-13 11:39:42 -0300

    feat: add isAdmin middleware to user routes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -2,8 +2,9 @@ const express = require("express")
 const route = express.Router()
 const usersController = require("../controllers/usersControllers")
 const authMiddleware = require("../middlewares/authMiddleware")
+const isAdminMiddleware = require("../middlewares/isAdminMiddleware")
 
-route.get("/allusers", authMiddleware, usersController.allUsersGet)
+route.get("/allusers", authMiddleware, isAdminMiddleware, usersController.allUsersGet)
 
 
 module.exports = route
