9a5cc014d2c3f8f759e189ba8b4fb815448a2025
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-13 11:29:57 -0300

    feat: add middleware to verify admin user (isAdmin)

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 function confirmAuthentication (req, res, next) {
     if (req.session && req.session.user) {
         res.locals.user = req.session.user
+        console.log(req.session.user.usertype)
         return next()
     } else {
         res.locals.user = null
