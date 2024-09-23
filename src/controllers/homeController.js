3bb4a62c162fa890299bf098279050366471b3cd
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-09-23 12:19:20 -0300

    feat: add session global middleware

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,9 @@ const bcrypt = require("bcryptjs/dist/bcrypt")
 const UserModel = require("../models/userModel")
 
 exports.home = (req, res) => {
-    res.render("index.hbs")
+    res.render("index.hbs", {
+        session: req.session
+    })
 }
 
 exports.homePost = async (req, res) => {
@@ -14,7 +16,9 @@ exports.homePost = async (req, res) => {
             req.session.user = user
             res.redirect("/allcars")
     } else { 
+        req.session.incorrectAuthMsg = "O username e/ou senha inseridos estão incorretos.Tente novamente"
         res.redirect("/")
+        req.session.incorrectAuthMsg = null
     }
     } catch (error) {
         res.redirect("/")
