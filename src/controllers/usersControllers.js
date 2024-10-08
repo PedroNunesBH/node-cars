6ed4ac19fc8d9d3c653538c4268a4542529f4078
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-10-08 11:33:59 -0300

    feat: add employee detail view page

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -6,3 +6,15 @@ exports.allUsersGet = (async (req, res) => {
     const allUsersObjects = allUsers.map(user => user.toObject())
     res.render("allUsers", { allUsersObjects })
 })
+
+exports.userDetail = async (req, res) => {
+    const username = req.params.username;
+    const userMongo = await UserModel.findOne({username: username});
+    
+    if (userMongo != null) {
+        const user = userMongo.toObject();  // Corrigido: use userMongo.toObject()
+        res.render("userEdit", { user });
+    } else {
+        res.render("userEdit");
+    }
+};
